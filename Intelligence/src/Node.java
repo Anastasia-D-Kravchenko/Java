aeb2412fa97cd0bf5ffc7b0996ec3a145e9c6205
Anastasia Kravchenko <118124554+Anastasia-D-Kravchenko@users.noreply.github.com>
2025-01-14 23:34:07 +0100

    prepare for the test

--- Intelligence/src/Node.java
+++ Intelligence/src/Node.java
@@ -40,9 +40,33 @@ class Node {
         this.data = data;
         this.next = null;
     }
+    public Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+    public static int fun(Node n) {
+        int sum = n.data;
+        int count = 0;
+        while (n.next != null) {
+            n = n.next;
+            if (count < 2) {
+                sum += n.data;
+                count++;
+            }
+            count++;
+        }
+        return sum;
+    }
 }
 class SinglyLL {
     Node head;
+    public static void main(String[] args) {
+        Node n1 = new Node(1);
+        Node n2 = new Node(2, n1);
+        Node n3 = new Node(3, n2);
+        Node n4 = new Node(4, n3);
+        System.out.println(Node.fun(n4));
+    }
     public void add(int data) {
         Node newNode = new Node(data);
         if (head == null) {
